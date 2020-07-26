ae1ef3e02b1dcfc963ea663ce97387263fc39985
charanM1232 <mjcn0624@gmail.com>
2020-07-25 19:44:28 -0700

    disapearing pigs

--- Pig.js
+++ Pig.js
@@ -2,6 +2,25 @@ class Pig extends BaseClass {
   constructor(x, y){
     super(x,y,50,50);
     this.image = loadImage("sprites/enemy.png");
+    this.visibility = 255
   }
 
+display(){
+  console.log(this.body.speed);
+  
+  if (this.body.speed<2.5){
+  super.display();
+} else {
+
+
+  World.remove(world,this.body);
+  push();
+  this.visibility = this.visibility-5;
+  tint(255,this.visibility)
+  image(this.image,this.body.position.X,this.body.position.Y,50,50);
+  pop();
+}
+
+}
+  
 };
--- sketch.js
+++ sketch.js
@@ -23,12 +23,12 @@ function setup(){
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
+    pig1 = new Pig(810, 355);
     log1 = new Log(810,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+    pig3 = new Pig(810, 225);
 
     log3 =  new Log(810,180,300, PI/2);
 
@@ -74,4 +74,12 @@ function mouseDragged(){
 
 function mouseReleased(){
     slingshot.fly();
+}
+
+function keyPressed(){
+
+    if(keyCode === 32){
+        Matter.Body.setPosition(bird.body, {x: 200, y:200});
+        slingshot.attach(bird.body);
+    }
 }
